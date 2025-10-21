570014b5a9a85d8c02c668ca9a758c8a8dd43a64
MiguelAnPeGr <a3830@lake.edu.mx>
2025-10-21 12:58:35 -0600

    1.-Suma de tres numeros

---
+++ 1.Suma de tres numeros.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main(){
+
+float N1;
+float N2;
+float N3;
+float R;
+
+printf("Ingresa tu primer numero a sumar: ");
+scanf("%f",&N1);
+printf("Ingresa tu segundo numero a sumar: ");
+scanf("%f",&N2);
+printf("Ingresa tu segundo numero a sumar: ");
+scanf("%f",&N3);
+
+R=N1+N2+N3;
+
+printf("Tu resultado es: %.2f",R);
+
+}
