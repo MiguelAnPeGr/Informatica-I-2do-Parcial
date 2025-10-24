13a7fef0524912aaf05567111d3ebfa8db0ee492
MiguelAnPeGr <a3830@lake.edu.mx>
2025-10-24 09:59:49 -0600

    4. Cuadrado de un numero

---
+++ Cuadrado de un numero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+
+float N1;
+float N2;
+
+printf("Ingresa el numero que quieres multiplicar por si mismo:\n");
+scanf("%f",&N1);
+
+N2=N1*N1;
+
+printf("El cuadrado de tu numero es:\n%f",N2);
+
+}
