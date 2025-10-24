fd31215aea61231648de064432ab7e6e090db92c
MiguelAnPeGr <a3830@lake.edu.mx>
2025-10-24 09:52:13 -0600

    2. Centimetros a Pulgadas

---
+++ Centimetros a Pulgadas.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+
+float cm;
+float pl;
+
+printf("Ingresa tu distancia en centimetros:\n");
+scanf("%f",&cm);
+
+pl=cm*0.3937;
+
+printf("Tu distancia en pulgadas es: \n %f",pl);
+
+}
