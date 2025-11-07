2cf806945a24343c31678d80f05911a027581fda
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-07 08:42:30 -0600

    6. Kilometros a Millas

---
+++ Km a Millas.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+
+float km;
+float mi;
+
+printf("Ingresa tu distamcia em Kilometros:\n");
+scanf("%f",&km);
+
+mi=km*0.621371;
+
+printf("Tu distancia en Millas es: %f", mi);
+
+}
