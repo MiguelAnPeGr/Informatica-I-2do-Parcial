717768fc2ec37b12008caa2a594d933133010687
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 10:01:06 -0600

    20. Promedio Aprobado y Exentado

--- Promedio de cuatro materias.c
+++ Promedio de cuatro materias.c
@@ -1,27 +1,32 @@
 #include<stdio.h>
-main(){
+main()
+{
 
-float Promedio;
-float CalMate;
-float CalIngles;
-float CalFrans;
-float CalEtica;
+    float Promedio;
+    float CalMate;
+    float CalIngles;
+    float CalFrans;
 
-printf("Ingresa tu calificacion en Matematicas:\n");
-scanf("%f",&CalMate);
-printf("Ingresa tu calificacion en Ingles:\n");
-scanf("%f",&CalIngles);
-printf("Ingresa tu calificacion en Frances:\n");
-scanf("%f",&CalFrans);
-printf("Ingresa tu calificacion en Etica:\n");
-scanf("%f",&CalEtica);
+    printf("Ingresa tu calificacion en Matematicas:\n");
+    scanf("%f",&CalMate);
+    printf("Ingresa tu calificacion en Ingles:\n");
+    scanf("%f",&CalIngles);
+    printf("Ingresa tu calificacion en Frances:\n");
+    scanf("%f",&CalFrans);
 
-Promedio=(CalMate+CalIngles+CalEtica+CalFrans)/4;
+    Promedio=(CalMate+CalIngles+CalFrans)/3;
 
-if(Promedio<6){
-        printf("Tu promedio total es de %f REPROBASTE",Promedio);
-}else{
-    printf("Tu promedio total es de %f. APROBASTE",Promedio);
-}
+    if(Promedio<6)
+    {
+        printf("Tu promedio total es de %f REPROBASTE Te vas a Extraordinario",Promedio);
+    }
+    if(Promedio>=6)
+    {
+        printf("Tu promedio total es de %f. APROBASTE",Promedio);
+    }
+    if(Promedio>=9)
+    {
+        printf("Tu promedio total es de %f. EXENTASTE",Promedio);
+    }
 
 }
