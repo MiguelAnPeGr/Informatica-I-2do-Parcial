5ecda0c6f7ea19cc303e1edf8841645d6f53c0a6
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 09:03:40 -0600

    12. Multiplicacion de tres numeros

---
+++ Multiplicacion de tres numeros.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+
+float N1;
+float N2;
+float N3;
+float N4;
+
+printf("Ingresa tu primer numero");
+scanf("%f",&N1);
+
+printf("Ingresa tu segundo numero");
+scanf("%f",&N2);
+
+printf("Ingresa tu tercer numero");
+scanf("%f",&N3);
+
+N4=N1*N2*N3;
+
+printf("Tu resultado es %f", N4);
+}
