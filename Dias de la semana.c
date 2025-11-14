77c7e50ac4c3f04f10547c8202fa683d1cfa5bac
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 11:12:07 -0600

    28. Meses del Ano

--- Dias de la semana.c
+++ Dias de la semana.c
@@ -4,39 +4,59 @@ main()
 
     int ND;
 
-    printf("Ingresa un numero del 1 al 7\n");
+    printf("Ingresa un numero del 1 al 12\n");
     scanf("%d",&ND);
 
     if(ND==1)
     {
-        printf("El dia selecionado es Lunes");
+        printf("El Mes de Enero");
     }
     if(ND==2)
     {
-        printf("El dia selecionado es Martes");
+        printf("El Mes de febrero");
     }
     if (ND==3)
     {
-        printf("El dia selecionado es Miercoles");
+        printf("El Mes de Marzo");
     }
     if (ND==4)
     {
-        printf("El dia selecionado es Jueves");
+        printf("El Mes de Abril");
     }
     if(ND==5)
     {
-        printf("El dia selecionado es Viernes");
+        printf("El Mes de Mayo");
     }
     if(ND==6)
     {
-        printf("El dia selecionado es Sabado");
+        printf("El Mes de Junio");
     }
     if(ND==7)
     {
-        printf("El dia selecionado es Domingo");
+        printf("El Mes de Julio");
     }
-    if(ND>7)
+    if(ND==8)
     {
-        printf("Error de datos");
+        printf("El Mes de Agosto");
+    }
+    if(ND==9)
+    {
+        printf("El Mes de Septiembre");
+    }
+    if (ND==10)
+    {
+        printf("El Mes de Octubre");
+    }
+    if (ND==11)
+    {
+        printf("El Mes de Noviembre");
+    }
+    if(ND==12)
+    {
+        printf("El Mes de Diciembre1");
+    }
+    if(ND>12)
+    {
+        printf("Error de datos ");
     }
 }
