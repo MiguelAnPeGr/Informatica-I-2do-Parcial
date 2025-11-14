52fae51e3e70fbee092f31725395f9482459e874
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 09:19:13 -0600

    15. Edad del usuario

---
+++ Edad del usuario.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+
+int AN;
+int AA;
+int ED;
+
+printf("Ingresa tu ano de nacimiento:\n");
+scanf("%d",&AN);
+
+printf("Ingresa el ano actual:\n");
+scanf("%d",&AA);
+
+ED=AA-AN;
+
+printf("Tu tienes %d anos", ED);
+}
