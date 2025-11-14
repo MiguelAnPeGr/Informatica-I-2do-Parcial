f57a52a94cb76701876749ccca7600dd08fce8cd
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 09:07:33 -0600

    13. Costo del terreno

---
+++ Costo del terreno.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+
+float M;
+float R;
+
+printf("Ingresa los metros cuadrados de tu terreno");
+scanf("%f",&M);
+
+R=M*1500;
+
+printf("El costpo de tu terreno es de: %f", R);
+}
