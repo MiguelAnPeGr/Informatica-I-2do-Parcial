aafb8c55daf9e9b1a84a29d7598e2b7fe29d86e7
MiguelAnPeGr <a3830@lake.edu.mx>
2025-11-14 09:28:50 -0600

    17. Cero o no Cero

---
+++ cero o no cero.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+
+    int N;
+
+    printf("Ingresa tu numero:\n");
+    scanf("%d",&N);
+
+    if(N==0)
+    {
+        printf("Tu numero es Cero");
+    }
+    else
+    {
+        printf("tu numero no es cero");
+    }
+}
+
